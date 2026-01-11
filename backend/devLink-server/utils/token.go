a55f173832585fa520fed610e79ff33b9e334bf0
Ayush Mehta <ayushmehtabps@gmail.com>
2026-01-11 21:34:15 +0530

    jwt_tokwn

---
+++ backend/devLink-server/utils/token.go
@@ -0,0 +1,37 @@
+package utils
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+
+type JWTClaims struct{
+	UserID string `json:"user_id"`
+	Email string `json:"email"`
+	Role string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+
+func GenerateToken(userId,email,role string)(string,error){
+
+	secret:=os.Getenv("JWT_SECRET");
+
+
+	claims:=JWTClaims{
+		UserID: userId,
+		Email: email,
+		Role: role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24*time.Hour)),
+			IssuedAt: jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token:=jwt.NewWithClaims(jwt.SigningMethodHS256,claims)
+
+	return token.SignedString([]byte(secret))
+}
