4f127d01f39d730e4ad4e2c45cab096afbb3b3a8
Ayush Mehta <ayushmehtabps@gmail.com>
2026-01-17 18:15:34 +0530

    message model

--- backend/devLink-server/models/chat_model.go
+++ backend/devLink-server/models/chat_model.go
@@ -24,6 +24,13 @@ type ChatRoom struct{
 }
 
 
+type Message struct{
+	ID bson.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	RoomID bson.ObjectID `bson:"room_id" json:"room_id"`
+	SenderID bson.ObjectID `bson:"sender_id" json:"sender_id"`
+	Content string `bson:"content" json:"content"`
+	CreatedAt time.Time `bson:"created_at" json:"created_at"`
+}
 
 
 
